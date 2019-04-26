baa72732cb5efd7d6982ca42911143f85af715f1
Thomas Yu <thomasyu888@gmail.com>
2019-04-26 12:10:00 -0700

    Remove xframe, and add shiny

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -147,7 +147,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'dj_pagination.middleware.PaginationMiddleware',
 ]
 
